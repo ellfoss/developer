59edc8db4a2300d85571d1dd2a951b15d73cf8a1
ellfoss <ellfoss43@gmail.com>
2016-10-19 15:24:17 +0300

    Commit

--- app/index.php
+++ app/index.php
@@ -9,24 +9,9 @@
 define('DIRSEP', DIRECTORY_SEPARATOR);
 
 $site_path = dirname(dirname(__FILE__)) . DIRSEP;
-//define('site_path', $site_path);
-define('site_path', '');
 
-function __autoload($class_name)
-{
-	$filename = strtolower($class_name) . '.php';
-	$file = site_path . 'classes' . DIRSEP . $filename;
-	if (file_exists($file) == false) return false;
-	include($file);
-	return true;
-}
-
-echo 'My site';
-
-//$registry = new Registry;
-//$router = new Router();
-//$registry['router'] = $router;
-//$router->setPath(site_path . 'controllers');
-//$router->delegate();
-//$template = new Template($registry);
-//$registry['template'] = $template;
+$error = file_get_contents('errors' . DIRSEP . 'error-404.html');
+$page = file_get_contents('main.html');
+$query = $_SERVER['QUERY_STRING'];
+if ($query != '') echo $error;
+else echo $page;
---
+++ app/scripts/error.js
@@ -0,0 +1,27 @@
+var curLetter = -1;
+var curNumber = 10;
+
+$(document).ready(function () {
+	setTimeout(animate404, 1400);
+	setInterval(setNumber, 1000);
+});
+
+function animate404() {
+	setInterval(animateLetter, 150);
+}
+
+function animateLetter() {
+	curLetter++;
+	if (curLetter > 8) curLetter = 0;
+	$('.anim').eq(curLetter).animate({top: -10}, 150, function () {
+		$(this).animate({top: 0}, 150);
+	});
+}
+
+function setNumber() {
+	curNumber--;
+	$('.number').text(curNumber).css({transform: 'scale(1.2, 1.2)'}).animate({opacity: 1}, 100, function () {
+		$(this).css({transform: 'scale(1, 1)'});
+	});
+	if (curNumber < 1) location.href = '/';
+}
