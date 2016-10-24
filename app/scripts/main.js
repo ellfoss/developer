b6cc1c0c5178684160696e18abf992f69fc0b496
ellfoss <ellfoss43@gmail.com>
2016-10-24 14:26:43 +0300

    main page menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	$('.menu-item').click(function () {
+		var last_menu = $('.menu-item.active').attr('menu');
+		var menu = $(this).attr('menu');
+		if (menu != last_menu) {
+			$('.menu-item').removeClass('active');
+			$(this).addClass('active');
+			var width = $(window).width();
+			if (width > 600) {
+				if (menu == 'portfolio') {
+					width = $('.sheet[name="main"]').outerWidth();
+					$('.main .wrapper').css({left: -width});
+				} else {
+					$('.main .wrapper').css({left: 0});
+				}
+			}
+		} else {
 
+		}
+	});
 });
