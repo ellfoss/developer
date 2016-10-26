8b422fe28b77d5219e4d7dcae0112e24c20bab9b
ellfoss <ellfoss43@gmail.com>
2016-10-26 10:08:54 +0300

    main page recode adaptive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,22 +17,14 @@ $(document).ready(function () {
 	});
 });
 
-function clickMenu(menu) {
-	var menu = menu || $('.menu-item:first-child').attr('menu');
-	var last_menu = $('.menu-item.active').attr('menu');
-	$('.menu-item').removeClass('active');
-	$(this).addClass('active');
-	if (menu != last_menu) {
-		var main_wrapper = $('.main .wrapper');
-		width = main_wrapper.outerWidth();
-		if (menu == 'portfolio') main_wrapper.css({left: -width});
-		else {
-			main_wrapper.css({left: 0});
-			var text_wrapper = $('.sheet-text-wrapper');
-			width = text_wrapper.outerWidth();
-			if (menu == 'contacts') text_wrapper.css({left: -width});
-			else text_wrapper.css({left: 0});
-		}
+function clickMenu(m) {
+	var menu = m || $('.menu-item:first-child').attr('menu');
+	$('.menu-item,.sheet,.view').removeClass('active');
+	$('.menu-item[menu="' + menu + '"]').addClass('active');
+	if (menu == 'portfolio') $('.sheet[name="portfolio"]').addClass('active');
+	else {
+		$('.sheet[name="main"]').addClass('active');
+		$('.view[name="' + menu + '"]').addClass('active');
 	}
 }
 
